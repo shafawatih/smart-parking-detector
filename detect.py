a3e8548159d2901c2758e38d9bebdcca7ba1e791
shafawatihsaleh <shafawatih7012@gmail.com>
2024-05-22 15:53:59 +0800

    smart-parking-detecto

--- detect.py
+++ detect.py
@@ -70,7 +70,7 @@ from utils.torch_utils import select_device, smart_inference_mode
 def run(
     weights=ROOT / "yolov5s.pt",  # model path or triton URL
     source=ROOT / "data/images",  # file/dir/URL/glob/screen/0(webcam)
-    data=ROOT / "data/coco128.yaml",  # dataset.yaml path
+    data=ROOT / "data/customdata.yaml",  # dataset.yaml path
     imgsz=(640, 640),  # inference size (height, width)
     conf_thres=0.25,  # confidence threshold
     iou_thres=0.45,  # NMS IOU threshold
@@ -269,7 +269,7 @@ def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument("--weights", nargs="+", type=str, default=ROOT / "yolov5s.pt", help="model path or triton URL")
     parser.add_argument("--source", type=str, default=ROOT / "data/images", help="file/dir/URL/glob/screen/0(webcam)")
-    parser.add_argument("--data", type=str, default=ROOT / "data/coco128.yaml", help="(optional) dataset.yaml path")
+    parser.add_argument("--data", type=str, default=ROOT / "data/customdata.yaml", help="(optional) dataset.yaml path")
     parser.add_argument("--imgsz", "--img", "--img-size", nargs="+", type=int, default=[640], help="inference size h,w")
     parser.add_argument("--conf-thres", type=float, default=0.25, help="confidence threshold")
     parser.add_argument("--iou-thres", type=float, default=0.45, help="NMS IoU threshold")
---
+++ train_data/README.dataset.txt
@@ -0,0 +1,6 @@
+# Smart Parking  > 2024-05-21 10:52pm
+https://universe.roboflow.com/smart-parking-detection/smart-parking-ltfyz
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
